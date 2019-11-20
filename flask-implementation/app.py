826767a67713e133f84b9cd899d75d9efef9458e
bischmlb <mathiasbischo@gmail.com>
2019-11-20 13:01:26 +0100

    retrieve userinfo

--- flask-implementation/app.py
+++ flask-implementation/app.py
@@ -1,9 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
+import os
 
+
+app.debug = True
+app.config['SECRET_KEY'] = os.urandom(12)
 
 @app.route('/')
-def hello():
+def index():
+    return render_template('login.html')
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.form.get('username')
+    password = request.form.get('password')
+    print(username)
+    print(password)
     return render_template('login.html')
 
 if __name__ == '__main__':
