21e4b8e39564ca290c3cbf053fa79f5924eb1e1f
bischmlb <mathiasbischo@gmail.com>
2019-11-20 13:08:41 +0100

    sample login function

--- flask-implementation/app.py
+++ flask-implementation/app.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 import os
 
+knownUsers = {
+"troels": "have",
+"boller": "kohberg"
+}
 
 app.debug = True
 app.config['SECRET_KEY'] = os.urandom(12)
@@ -15,6 +19,14 @@ def index():
 def login():
     username = request.form.get('username')
     password = request.form.get('password')
+    if username in knownUsers:
+        if knownUsers[username] == password:
+            print("login successful!!!")
+        else:
+            print("password wrong")
+    else:
+        print("user not recognized")
+
     print(username)
     print(password)
     return render_template('login.html')
